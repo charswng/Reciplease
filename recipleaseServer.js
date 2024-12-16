a033a57e360f6ac883e00b8fe489839eed92923e
Charis Wang <charswng@gmail.com>
2024-12-15 20:35:13 -0500

    Fixed function + env

--- recipleaseServer.js
+++ recipleaseServer.js
@@ -10,12 +10,12 @@ const { MongoClient } = require('mongodb');
 process.stdin.setEncoding("UTF8");
 
 
-require("dotenv").config({path : path.resolve(__dirname, '/.env') })
+require("dotenv").config({path : path.resolve(__dirname, '.env') })
 const username = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
 const dbAndCollection = {db_name: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
 
-const uri = `mongodb+srv://${username}:${password}@cluster0.3vnum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = process.env.MONGO_CONNECTION_STRING;
 const client = new MongoClient(uri);
 
 // Initialize Express application
@@ -81,7 +81,7 @@ async function insertUser(name) {
         await client
             .db(dbAndCollection.db_name)
             .collection(dbAndCollection.collection)
-            .insertOne(name);
+            .insertOne({ name: name });
     } catch (error) {
         console.error(error);
     } finally {
